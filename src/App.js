9fb83f376b0fa14c4e868728e3ee0569cc3d7070
mac <mac@macs-MacBook-Pro.local>
2024-02-09 08:03:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <ProductCard/>
     </div>
   );
 }
